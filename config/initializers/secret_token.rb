d479ed853b1d4b15f584e24d0803d4bcbbc24ac7
Chris Beer <chris@cbeer.info>
2011-10-24 17:34:52 -0400

    initial commit
    This is a demo application that will go through the steps of setting
    up a new Blacklight application with a custom metadata schema. We
    will start from a bare Rails application, build functionality using
    ActiveRecord and the database, and then introduce Blacklight and
    Solr in a second phase.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlacklightOaiHarvesterDemo::Application.config.secret_token = '0786863e4a34da8cc9e5791f8838a5231d50d3502677a0a14ec849c7bad453f88975b2af2b3fd6b56e74e9858ebb1801f93d1ea7ba26f2d1e78db7cabad52b98'
