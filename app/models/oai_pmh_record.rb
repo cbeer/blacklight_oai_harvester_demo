bd4f8caa33b45e45a7984bf3d85b712e048a7d33
Chris Beer <chris_beer@wgbh.org>
2011-10-25 12:13:16 -0400

    Split OAI-PMH record metadata from the payload
    We want to think about supporting generic OAI-PMH provided content. The
    structure of an OAI-PMH document is defined in the specification, but
    the payload of <metadata> is provider specific.
    
    Here, we create the model OaiPmhRecord, which can be mixed into any
    class that provides the appropriate terminology helpers:
    
    .set_oai_metadata_terminology : add metadata-specific terminology
    
    .add_oai_metadata_terminology_proxies : add top-level terminology
    accessors to the OAI document

---
+++ app/models/oai_pmh_record.rb
@@ -0,0 +1,33 @@
+module OaiPmhRecord
+  extend ActiveSupport::Concern
+
+  included do
+    extend ClassAttributes
+    include OM::XML::Document
+    set_terminology do |t|
+      t.root(:path => 'record', :namespace_prefix => nil)
+
+      t.header(:namespace_prefix => nil) do
+        t.identifier :namespace_prefix => nil
+        t.datestamp :namespace_prefix => nil
+        t.setSpec :namespace_prefix => nil
+      end
+
+      t.metadata(:namespace_prefix => nil) do
+        set_oai_metadata_terminology(t)
+      end
+
+      add_oai_metadata_terminology_proxies(t)
+    end
+  end
+
+  module ClassAttributes
+    def set_oai_metadata_terminology terminology
+
+    end
+
+    def add_oai_metadata_terminology_proxies terminology
+
+    end
+  end
+end
